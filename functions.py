f5c78740a5f528585907d736437f82f7a89564bb
Jonathan Samuels <jonathansamuels@Jonathans-MacBook-Pro-2.local>
2024-06-12 17:58:55 -0700

    Basic Function Execise

---
+++ functions.py
@@ -0,0 +1,15 @@
+# TODO 1: Add two parameters (length_ft and width_ft)
+def calc_square_meters_from_feet(length_ft, width_ft):
+    
+    
+    # TODO 2: Modify the code below:
+    width = 0.3048 * width_ft
+    length = 0.3048 * length_ft
+    metric_area = width * length
+    
+    
+    
+    
+
+    # Leave the line below as it is
+    return metric_area
